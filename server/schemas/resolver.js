83797131a385fd921c48a2da912bd054d239c015
Neftali <nperc001@ucr.edu>
2023-12-20 16:52:48 -0800

    added resolvers and index files

---
+++ server/schemas/resolver.js
@@ -0,0 +1,75 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async (parent, { newBook }, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        const updatedUsed = await  User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { saveBooks: book },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+        return updatedUsed;
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw AuthenticationError;
+    },
+
+    // Make it so a logged in user can only remove a skill from their own profile
+    removeBooks: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { bookId: bookId } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
