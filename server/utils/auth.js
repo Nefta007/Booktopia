6fc2ae211871a3acf65a6eab18dd2bec7c58a048
Neftali <nperc001@ucr.edu>
2023-12-20 16:11:36 -0800

    updated auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,12 +1,18 @@
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,7 +22,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;//res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
